50846cb0dbb31acca3a05021e31abfd52a37310e
JakeTrapnell <trapnelljake@gmail.com>
2019-01-11 15:58:29 +0000

    added squares, board and game. that when combined create a 3x3 grid that can be clicked to add crosses

---
+++ src/Header.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+//functional component. To be used if no other methods.
+const Header = (props) => (<div><h1>{props.headerProp}</h1></div>);
+
+export default Header;
