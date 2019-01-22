d9830954e0e0f9194db1c9efa179c1c95907683c
JakeTrapnell <trapnelljake@gmail.com>
2019-01-22 18:10:15 +0000

    name enetered now returned dynamicly

--- src/CrudButtons.js
+++ src/CrudButtons.js
@@ -4,7 +4,21 @@ import ReadComp from  './ReadComp'
 import UpdateComp from  './UpdateComp'
 import DeleteComp from  './DeleteComp'
 
+let userInput;
+
 class CrudButtons extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={name: ""};
+    
+    }
+
+    update =(event)=>{
+        userInput = event.target.value;
+        console.log(userInput);
+        this.setState({name: userInput});
+    }
 
     render()
     {
@@ -15,14 +29,20 @@ class CrudButtons extends Component{
             <br/>
             <br/>
             <form>
-                Name:<br/>
-                <input></input>
+                Name:
+                <br/>
+                <input id="userInput" type="text" placeholder="Enter your name" onChange={this.update}/>
             </form>
+            <br/>
+            <p>Name: {this.state.name}</p>
             <br/>
             <CreateComp/>
             <ReadComp/>
             <UpdateComp/>
             <DeleteComp/>
+
+
+
             </div>
 
         );
